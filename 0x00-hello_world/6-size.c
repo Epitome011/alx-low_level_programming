ef59de4934eb84dda33e1763b3846ee3e5451281
Darlington Obioha <epitome011@yahoo.com>
2022-12-08 14:53:33 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - print out sizes of data types in C
- * code by Epitome
- * Return: 0
+ * main - Entry point
+ * 
+ * Return: Always 0 (success)
  */
 int main(void)
 {
